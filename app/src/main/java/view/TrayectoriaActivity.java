44028fc3bfe838be3c27dabb8305d7dedf0a854e
driverad423 <diegorivera423@gmail.com>
2020-10-28 22:44:14 -0500

    Initial commit

---
+++ app/src/main/java/view/TrayectoriaActivity.java
@@ -0,0 +1,14 @@
+package view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrayectoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
